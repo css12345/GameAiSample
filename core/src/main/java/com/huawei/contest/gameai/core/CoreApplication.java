8be1bdb04c33886e8aad79cde2c6064fe1cdc9cf
css12345 <2318018219@qq.com>
2026-06-28 02:04:50 +0800

    增加启动类

---
+++ core/src/main/java/com/huawei/contest/gameai/core/CoreApplication.java
@@ -0,0 +1,37 @@
+package com.huawei.contest.gameai.core;
+
+import com.huawei.contest.gameai.base.client.GameClient;
+import com.huawei.contest.gameai.base.client.GameHolder;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.Optional;
+
+@Slf4j
+@SpringBootApplication
+public class CoreApplication implements CommandLineRunner {
+    public static void main(String[] args) {
+        SpringApplication.run(CoreApplication.class, args);
+    }
+
+    @Override
+    public void run(String[] args) {
+        GameClient gameClient = null;
+        try {
+            String ip = args[1];
+            int port = Integer.parseInt(args[2]);
+            int playerId = Integer.parseInt(args[0]);
+            gameClient = GameClient.connect(ip, port).withPlayer(playerId, "Test Red", "v1.0");
+            gameClient.start();
+            while (!GameHolder.isGameOver()) {
+                Thread.sleep(500);
+            }
+        } catch (InterruptedException e) {
+            log.error("Sleep is interrupted");
+        } finally {
+            Optional.ofNullable(gameClient).ifPresent(GameClient::stop);
+        }
+    }
+}
